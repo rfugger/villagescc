8e95e6190ed874e7f9b4ab83169413067554c50f
Ryan Fugger <arv@ryanfugger.com>
2011-10-18 11:02:16 -0700

    Handle instruction inputs that are held over as input values across page reloads.

--- cc/static/js/common.js
+++ cc/static/js/common.js
@@ -3,12 +3,12 @@
 function init_instruction_input() {
 	/* Allow search boxes to give instructions that disappear when selected. */
 	$('.instruction_input').each(function(index) {
-		// Only apply help instruction if blank.
-		if ($(this).val() != '')
-			return;
 		// Input instruction goes in 'help' attribute.
 		var orig_text = $(this).attr('help');
-		$(this).val(orig_text);
+
+		// Only apply help instruction if blank.
+		if ($(this).val() == '')
+			$(this).val(orig_text);
 
 		$(this).focus(function() {  // Remove instruction on focus.
 			if ($(this).val() == orig_text) {
@@ -19,7 +19,8 @@ function init_instruction_input() {
 				$(this).val(orig_text);
 			}
 		});
-		// Remove on form submit.
+		
+		// Remove help on form submit.
 		input = $(this);  // Inside form submit handler, $(this) = form.
 		input.closest('form').submit(function() {
 			if (input.val() == orig_text) {
