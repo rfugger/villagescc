7e3c5432723f39b3fa4786f3a0dac5d9f2b1d8a5
Ryan Fugger <arv@ryanfugger.com>
2011-10-26 00:13:58 -0700

    Handle no profile.

--- cc/profile/middleware.py
+++ cc/profile/middleware.py
@@ -1,7 +1,12 @@
+from cc.profile.models import Profile
+
 class ProfileMiddleware(object):
     def process_request(self, request):
         "Populate request with user profile, for convenience."
+        request.profile = None
         if request.user.is_authenticated():
-            request.profile = request.user.profile
-        else:
-            request.profile = None
+            try:
+                request.profile = request.user.profile
+            except Profile.DoesNotExist:
+                pass
+
