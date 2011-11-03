7dbb5ebbc7bb7a7588057387a33f8e9f2f268535
Ryan Fugger <arv@ryanfugger.com>
2011-11-02 17:36:41 -0700

    Re-order about section to put motivation at top.

--- cc/pages/urls.py
+++ cc/pages/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic.simple import direct_to_template, redirect_to
 
 urlpatterns = patterns(
     'cc.pages.views',
     url(r'^$', 'intro', name='home'),
     url(r'^feedback/$', 'feedback', name='feedback'),
-    url(r'^about/$', direct_to_template,
-        {'template': 'how_it_works.html'}, name='about'),
+    url(r'^about/$', redirect_to, {'url': '/about/motivation/'}, name='about'),
+    url(r'^about/how/$', direct_to_template,
+        {'template': 'how_it_works.html'}, name='how_it_works'),
     url(r'^about/privacy/$', direct_to_template,
         {'template': 'privacy.html'}, name='privacy'),
     url(r'^about/motivation/$', direct_to_template,
