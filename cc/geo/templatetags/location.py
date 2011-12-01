7c2f32d9eec84715e93b7981c22bcbcca8b3452f
Ryan Fugger <arv@ryanfugger.com>
2011-12-01 15:16:13 -0800

    Fix bug in relative location.

--- cc/geo/templatetags/location.py
+++ cc/geo/templatetags/location.py
@@ -5,16 +5,19 @@ register = template.Library()
 @register.simple_tag
 def relative_location(loc, user_loc):
     "Returns a short useful description of `loc` for someone at `user_loc`."
-    # In same city if city names are identical and points are within 40km.
+    # In same city if city names are identical and points are within 40km:
+    # Just give neighborhood.
     if (loc.city.lower() == user_loc.city.lower() and
         loc.point.distance(user_loc.point) < 40000):
         loc_str = loc.neighborhood
     else:  # Different city.
-        if loc.country == user_loc.country:
-            if loc.state == user_loc.state:
-                loc_str = loc.city
-            else:
-                loc_str = u"%s, %s" % (loc.city, loc.state)
-        else:
-            loc_str = u"%s, %s, %s" % (loc.city, loc.state, loc.country)
+        locs = []
+        if loc.city:
+            locs.append(loc.city)
+        if loc.state != user_loc.state or loc.country != user_loc.country:
+            if loc.state:
+                locs.append(loc.state)
+        if loc.country != user_loc.country:
+            locs.append(loc.country)  # Country can't be blank.
+        loc_str = ', '.join(locs)
     return loc_str
