3c85162062378228b04db9fb860c238bd214a6f7
Ryan Fugger <arv@ryanfugger.com>
2012-05-30 08:51:53 -0700

    Better profile admin.

--- cc/profile/admin.py
+++ cc/profile/admin.py
@@ -6,6 +6,19 @@ class SettingsInline(admin.StackedInline):
     model = Settings
 
 class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'user',
+        'location',
+        'created',
+        'updated',
+        'last_login',
+    )
+    ordering = ('-user__last_login',)
+    
     inlines = (SettingsInline,)
+
+    def last_login(self, profile):
+        return profile.user.last_login
 
 admin.site.register(Profile, ProfileAdmin)
