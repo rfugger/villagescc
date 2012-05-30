af6da65d2ea465bb1851e5046f4fc1441435625f
Ryan Fugger <arv@ryanfugger.com>
2012-05-30 08:38:03 -0700

    Nicer payment model admin.

--- cc/payment/admin.py
+++ cc/payment/admin.py
@@ -2,5 +2,34 @@ from django.contrib import admin
 
 from cc.payment.models import Payment, Entry
 
-admin.site.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'payer_name',
+        'recipient_name',
+        'amount',
+        'submitted_at',
+        'last_attempted_at',
+        'status',
+    )
+    list_filter = ('status',)
+    ordering = ('-submitted_at',)
+
+    def payer_name(self, payment):
+        return self._node_pretty_name(payment.payer)
+    
+    def recipient_name(self, payment):
+        return self._node_pretty_name(payment.recipient)
+
+    def _node_profile(self, node):
+        from cc.profile.models import Profile
+        try:
+            return Profile.objects.get(pk=node.alias)
+        except Profile.DoesNotExist:
+            return None
+
+    def _node_pretty_name(self, node):
+        profile = self._node_profile(node)
+        return profile and unicode(profile) or unicode(node)
+        
+admin.site.register(Payment, PaymentAdmin)
 admin.site.register(Entry)
